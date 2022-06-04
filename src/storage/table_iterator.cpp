fdd0b00261c7a3d00642950a5e5e05cd6f5fcce3
李玥颖 <3200102865@zju.edu.cn>
2022-06-04 06:33:34 +0000

    Replace table_iterator.cpp

--- src/storage/table_iterator.cpp
+++ src/storage/table_iterator.cpp
@@ -2,101 +2,108 @@
 #include "storage/table_iterator.h"
 #include "storage/table_heap.h"
 
-TableIterator::TableIterator() {
 
-}
+
+TableIterator:: TableIterator(TableHeap* table_heap,TablePage* page,RowId rid){
+    this->table_heap_ = table_heap;
+    this->rid_ = rid;
+    this->row_=new Row(rid_);
+    if (rid_.GetPageId() != INVALID_PAGE_ID) {
+      this->table_heap_->GetTuple(row_, nullptr);
+    }
+  };
 
 TableIterator::TableIterator(const TableIterator &other) {
   table_heap_=other.table_heap_;
-  current_page=other.current_page;
-  row_id=other.row_id;
-  current_row=new Row(row_id);
-  //current_row=other.current_row;//涉及指针的拷贝构造
+  page_=other.page_;
+  rid_=other.rid_;
+  row_=new Row(rid_);
 }
 
-TableIterator::TableIterator (TableHeap* table_heap,TablePage* cur_page,RowId row_id_){
-  table_heap_=table_heap;
-  current_page=cur_page;
-  row_id=row_id_;
-  current_row=new Row(row_id);
-}
-
-TableIterator::~TableIterator() {
-  delete current_row;
-}
-
-// bool TableIterator::operator==(const TableIterator &itr) const {
-//   return(table_heap_==itr.table_heap_&&row_id==itr.row_id);
-// }
-
-// bool TableIterator::operator!=(const TableIterator &itr) const {
-//   return !((*this)==itr);
-// }
-
-const Row &TableIterator::operator*() {
-  // auto page = reinterpret_cast<TablePage *>(table_heap_->buffer_pool_manager_->FetchPage(row_id.GetPageId()));
-  // //关于row相关的memheap的问题
-  // Row current_row(row_id);
-  // page->GetTuple(&current_row,table_heap_->schema_,nullptr,table_heap_->lock_manager_);
-  // //同样也是这个row的问题，
-
-  // //return nullptr;
-  return *current_row;
-  //ASSERT(false, "Not implemented yet.");
-}
+const Row &TableIterator::operator*() { return *(this->row_); }
 
 Row *TableIterator::operator->() {
-  //在哪里new 一个row呢，在哪里释放啊？？？
-  //需要new 一个空间吧，保存在一个vector中，在析构时释放，？？？
-  //传入一个memheap,然后等memheap 析构
-  return current_row;
+  return nullptr;
 }
 
 TableIterator &TableIterator::operator++() {
-  //在这一页有
-  TablePage* page= reinterpret_cast<TablePage *>(table_heap_->buffer_pool_manager_->FetchPage(row_id.GetPageId()));
-  RowId  next;
-  if(page->GetNextTupleRid(row_id,&next)){
-      delete current_row;
-      current_page=page;
-      table_heap_->buffer_pool_manager_->UnpinPage(page->GetPageId(),false);
-      current_row=new Row(next);
-      row_id=next;
-      return *this;
-  }
-  while(1){
-    //std::cout<<"next_page_id="<<row_id.GetPageId()<<std::endl;
-    page_id_t next_page=page->GetNextPageId();
-    std::cout<<"next_page_id="<<next_page<<std::endl;
-    if(next_page==INVALID_PAGE_ID){
-      current_page=nullptr;
-      row_id=INVALID_ROWID;
-      std::cout<<"1 success"<<std::endl;
-      delete current_row;
-      std::cout<<"delete success"<<std::endl;
-      break;
+  RowId rid = rid_;
+  auto page = reinterpret_cast<TablePage *>(this->table_heap_->buffer_pool_manager_->FetchPage(rid.GetPageId()));
+  ASSERT(page != nullptr,"Not found this page!");
+  RowId next_rid;
+  page->RLatch();
+  bool is_get=page->GetNextTupleRid(rid,&next_rid);
+  if (next_rid.GetPageId() != INVALID_PAGE_ID && is_get) {
+    this->row_->SetRowId(next_rid);
+    if(*this!=this->table_heap_->End()){
+      page->GetTuple(this->row_, this->table_heap_->schema_, nullptr, this->table_heap_->lock_manager_);
     }
-    page=reinterpret_cast<TablePage *>(table_heap_->buffer_pool_manager_->FetchPage(next_page));
-    if(page->GetFirstTupleRid(&next)){
-      delete current_row;
-      current_page=page;
-      table_heap_->buffer_pool_manager_->UnpinPage(page->GetPageId(),false);
-      current_row=new Row(next);
-      row_id=next;
-      break;
+    page->RUnlatch();
+    this->page_ = page;
+    this->table_heap_->buffer_pool_manager_->UnpinPage(page->GetTablePageId(), false);
+  } else {
+    auto next_page_id = page->GetNextPageId();
+    page->RUnlatch();
+    this->table_heap_->buffer_pool_manager_->UnpinPage(page->GetTablePageId(), false);
+    while(next_page_id!=INVALID_PAGE_ID){
+      auto next_page = reinterpret_cast<TablePage *>(this->table_heap_->buffer_pool_manager_->FetchPage(next_page_id));
+      next_page->RLatch();
+      if(next_page->GetFirstTupleRid(&next_rid)){
+        this->row_->SetRowId(next_rid);
+        if(*this!=this->table_heap_->End()){
+          next_page->GetTuple(this->row_, this->table_heap_->schema_, nullptr, this->table_heap_->lock_manager_);
+        }
+        next_page->RUnlatch();
+        this->table_heap_->buffer_pool_manager_->UnpinPage(next_page->GetTablePageId(), false);
+        this->page_ = next_page;
+        break;
+      }
+      next_page_id=next_page->GetNextPageId();
+      next_page->RUnlatch();
+      this->table_heap_->buffer_pool_manager_->UnpinPage(next_page->GetTablePageId(), false);
     }
   }
 
   return *this;
-  //忘记unpin了
-  //下一页找第一个元组
-  //table_iterator 的用处只能用来遍历，不能用来修改
 }
 
 TableIterator TableIterator::operator++(int) {
-  TablePage* old_page=current_page;
-  RowId old_id=row_id;
-  ++(*this);
-  if(this->row_id==INVALID_ROWID)std::cout<<"2 success"<<std::endl;
-  return TableIterator(this->table_heap_,old_page,old_id);
+  RowId rid = rid_;
+  auto page = reinterpret_cast<TablePage *>(this->table_heap_->buffer_pool_manager_->FetchPage(rid.GetPageId()));
+  ASSERT(page != nullptr,"Not found this page!");
+  TablePage* cur_page = page;
+  RowId next_rid;
+  page->RLatch();
+  bool is_get=page->GetNextTupleRid(rid,&next_rid);
+  if (next_rid.GetPageId() != INVALID_PAGE_ID && is_get) {
+    this->row_->SetRowId(next_rid);
+    if(*this!=this->table_heap_->End()){
+      page->GetTuple(this->row_, this->table_heap_->schema_, nullptr, this->table_heap_->lock_manager_);
+    }
+    page->RUnlatch();
+    this->page_ = page;
+    this->table_heap_->buffer_pool_manager_->UnpinPage(page->GetTablePageId(), false);
+  } else {
+    auto next_page_id = page->GetNextPageId();
+    page->RUnlatch();
+    this->table_heap_->buffer_pool_manager_->UnpinPage(page->GetTablePageId(), false);
+    while(next_page_id!=INVALID_PAGE_ID){
+      auto next_page = reinterpret_cast<TablePage *>(this->table_heap_->buffer_pool_manager_->FetchPage(next_page_id));
+      next_page->RLatch();
+      if(next_page->GetFirstTupleRid(&next_rid)){
+        this->row_->SetRowId(next_rid);
+        if(*this!=this->table_heap_->End()){
+          next_page->GetTuple(this->row_, this->table_heap_->schema_, nullptr, this->table_heap_->lock_manager_);
+        }
+        next_page->RUnlatch();
+        this->table_heap_->buffer_pool_manager_->UnpinPage(next_page->GetTablePageId(), false);
+        this->page_ = next_page;
+        break;
+      }
+      next_page_id=next_page->GetNextPageId();
+      next_page->RUnlatch();
+      this->table_heap_->buffer_pool_manager_->UnpinPage(next_page->GetTablePageId(), false);
+    }
+  }
+  return TableIterator(this->table_heap_,cur_page,rid);
 }
