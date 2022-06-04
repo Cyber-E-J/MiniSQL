f3145d37eac04a0ec6fc4da05eef2e30dfd5bffd
李玥颖 <3200102865@zju.edu.cn>
2022-06-04 15:20:58 +0000

    Replace index_iterator.cpp

--- src/index/index_iterator.cpp
+++ src/index/index_iterator.cpp
@@ -2,30 +2,58 @@
 #include "index/generic_key.h"
 #include "index/index_iterator.h"
 
-INDEX_TEMPLATE_ARGUMENTS INDEXITERATOR_TYPE::IndexIterator() {
 
+INDEX_TEMPLATE_ARGUMENTS INDEXITERATOR_TYPE::IndexIterator(LeafPage *lp,int x,BufferPoolManager *bpm){
+  leaf_page = lp;
+  index = x;
+  buffer_pool_manager = bpm;
 }
-
 INDEX_TEMPLATE_ARGUMENTS INDEXITERATOR_TYPE::~IndexIterator() {
-
+  //unpin这个page，不是脏页
+  buffer_pool_manager->UnpinPage(leaf_page->GetPageId(),false);
 }
 
 INDEX_TEMPLATE_ARGUMENTS const MappingType &INDEXITERATOR_TYPE::operator*() {
-  ASSERT(false, "Not implemented yet.");
+  return leaf_page->GetItem(index);
 }
 
 INDEX_TEMPLATE_ARGUMENTS INDEXITERATOR_TYPE &INDEXITERATOR_TYPE::operator++() {
-  ASSERT(false, "Not implemented yet.");
+  if (index==leaf_page->GetSize()-1){//是这个页的最后一个
+    page_id_t next_page_id = leaf_page->GetNextPageId();
+    if (next_page_id!=INVALID_PAGE_ID){//还有下一个页
+      index = 0;//index归零
+      //leaf_page变成下一页
+      Page* page = buffer_pool_manager->FetchPage(next_page_id);
+      leaf_page = reinterpret_cast<LeafPage *>(page->GetData());
+    }
+    else{
+      index ++;
+    }
+  }
+  else{
+    index ++;
+  }
+  return *this;
 }
 
 INDEX_TEMPLATE_ARGUMENTS
 bool INDEXITERATOR_TYPE::operator==(const IndexIterator &itr) const {
-  return false;
+  if (leaf_page->GetPageId()==(itr.leaf_page)->GetPageId()&&index==itr.index){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 INDEX_TEMPLATE_ARGUMENTS
 bool INDEXITERATOR_TYPE::operator!=(const IndexIterator &itr) const {
-  return false;
+  if (leaf_page->GetPageId()!=(itr.leaf_page)->GetPageId()||index!=itr.index){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 
 template
