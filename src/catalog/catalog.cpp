4850482c06d6ec70e42e3957a7585b1473549e09
李世钰 <3200102893@zju.edu.cn>
2022-06-05 10:44:46 +0000

    Replace catalog.cpp

--- src/catalog/catalog.cpp
+++ src/catalog/catalog.cpp
@@ -1,94 +1,118 @@
-#include "catalog/catalog.h"
+#ifndef MINISQL_CATALOG_H
+#define MINISQL_CATALOG_H
 
-void CatalogMeta::SerializeTo(char *buf) const {
-  // ASSERT(false, "Not Implemented yet");
-}
+#include <string>
+#include <map>
+#include <unordered_map>
 
-CatalogMeta *CatalogMeta::DeserializeFrom(char *buf, MemHeap *heap) {
-  // ASSERT(false, "Not Implemented yet");
-  return nullptr;
-}
+#include "buffer/buffer_pool_manager.h"
+#include "catalog/indexes.h"
+#include "catalog/table.h"
+#include "common/config.h"
+#include "common/dberr.h"
+#include "transaction/lock_manager.h"
+#include "transaction/log_manager.h"
+#include "transaction/transaction.h"
 
-uint32_t CatalogMeta::GetSerializedSize() const {
-  // ASSERT(false, "Not Implemented yet");
-  return 0;
-}
+class CatalogMeta {
+  friend class CatalogManager;
 
-CatalogMeta::CatalogMeta() {}
+public:
+  void SerializeTo(char *buf) const;
 
+  static CatalogMeta *DeserializeFrom(char *buf, MemHeap *heap);
 
-CatalogManager::CatalogManager(BufferPoolManager *buffer_pool_manager, LockManager *lock_manager,
-                               LogManager *log_manager, bool init)
-        : buffer_pool_manager_(buffer_pool_manager), lock_manager_(lock_manager),
-          log_manager_(log_manager), heap_(new SimpleMemHeap()) {
-  // ASSERT(false, "Not Implemented yet");
-}
+  uint32_t GetSerializedSize() const;
 
-CatalogManager::~CatalogManager() {
-  delete heap_;
-}
+  inline table_id_t GetNextTableId() const {
+    return table_meta_pages_.size() == 0 ? 0 : table_meta_pages_.rbegin()->first + 1;
+  }
 
-dberr_t CatalogManager::CreateTable(const string &table_name, TableSchema *schema,
-                                    Transaction *txn, TableInfo *&table_info) {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+  inline index_id_t GetNextIndexId() const {
+    return index_meta_pages_.size() == 0 ? 0 : index_meta_pages_.rbegin()->first + 1;
+  }
 
-dberr_t CatalogManager::GetTable(const string &table_name, TableInfo *&table_info) {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+  static CatalogMeta *NewInstance(MemHeap *heap) {
+    void *buf = heap->Allocate(sizeof(CatalogMeta));
+    return new(buf) CatalogMeta();
+  }
 
-dberr_t CatalogManager::GetTables(vector<TableInfo *> &tables) const {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+  /**
+   * Used only for testing
+   */
+  inline std::map<table_id_t, page_id_t> *GetTableMetaPages() {
+    return &table_meta_pages_;
+  }
 
-dberr_t CatalogManager::CreateIndex(const std::string &table_name, const string &index_name,
-                                    const std::vector<std::string> &index_keys, Transaction *txn,
-                                    IndexInfo *&index_info) {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+  /**
+   * Used only for testing
+   */
+  inline std::map<index_id_t, page_id_t> *GetIndexMetaPages() {
+    return &index_meta_pages_;
+  }
 
-dberr_t CatalogManager::GetIndex(const std::string &table_name, const std::string &index_name,
-                                 IndexInfo *&index_info) const {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+private:
+  explicit CatalogMeta();
 
-dberr_t CatalogManager::GetTableIndexes(const std::string &table_name, std::vector<IndexInfo *> &indexes) const {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+private:
+  static constexpr uint32_t CATALOG_METADATA_MAGIC_NUM = 89849;
+  std::map<table_id_t, page_id_t> table_meta_pages_;
+  std::map<index_id_t, page_id_t> index_meta_pages_;
+};
 
-dberr_t CatalogManager::DropTable(const string &table_name) {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+/**
+ * Catalog manager
+ *
+ */
+class CatalogManager {
+public:
+  explicit CatalogManager(BufferPoolManager *buffer_pool_manager, LockManager *lock_manager,
+                          LogManager *log_manager, bool init);
 
-dberr_t CatalogManager::DropIndex(const string &table_name, const string &index_name) {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+  ~CatalogManager();
 
+  dberr_t CreateTable(const std::string &table_name, TableSchema *schema, Transaction *txn, TableInfo *&table_info);
 
-dberr_t CatalogManager::FlushCatalogMetaPage() const {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+  dberr_t GetTable(const std::string &table_name, TableInfo *&table_info);
 
-dberr_t CatalogManager::LoadTable(const table_id_t table_id, const page_id_t page_id) {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+  dberr_t GetTables(std::vector<TableInfo *> &tables) const;
 
-dberr_t CatalogManager::LoadIndex(const index_id_t index_id, const page_id_t page_id) {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+  dberr_t CreateIndex(const std::string &table_name, const std::string &index_name,
+                      const std::vector<std::string> &index_keys, Transaction *txn,
+                      IndexInfo *&index_info);
 
-dberr_t CatalogManager::GetTable(const table_id_t table_id, TableInfo *&table_info) {
-  // ASSERT(false, "Not Implemented yet");
-  return DB_FAILED;
-}
+  dberr_t GetIndex(const std::string &table_name, const std::string &index_name, IndexInfo *&index_info) const;
+
+  dberr_t GetTableIndexes(const std::string &table_name, std::vector<IndexInfo *> &indexes) const;
+
+  dberr_t DropTable(const std::string &table_name);
+
+  dberr_t DropIndex(const std::string &table_name, const std::string &index_name);
+
+private:
+  dberr_t FlushCatalogMetaPage() const;
+
+  dberr_t LoadTable(const table_id_t table_id, const page_id_t page_id);
+
+  dberr_t LoadIndex(const index_id_t index_id, const page_id_t page_id);
+
+  dberr_t GetTable(const table_id_t table_id, TableInfo *&table_info);
+
+private:
+  [[maybe_unused]] BufferPoolManager *buffer_pool_manager_;
+  [[maybe_unused]] LockManager *lock_manager_;
+  [[maybe_unused]] LogManager *log_manager_;
+  [[maybe_unused]] CatalogMeta *catalog_meta_;
+  [[maybe_unused]] std::atomic<table_id_t> next_table_id_;
+  [[maybe_unused]] std::atomic<index_id_t> next_index_id_;
+  // map for tables
+  std::unordered_map<std::string, table_id_t> table_names_;
+  std::unordered_map<table_id_t, TableInfo *> tables_;
+  // map for indexes: table_name->index_name->indexes
+  [[maybe_unused]] std::unordered_map<std::string, std::unordered_map<std::string, index_id_t>> index_names_;
+  [[maybe_unused]] std::unordered_map<index_id_t, IndexInfo *> indexes_;
+  // memory heap
+  MemHeap *heap_;
+};
+
+#endif //MINISQL_CATALOG_H
