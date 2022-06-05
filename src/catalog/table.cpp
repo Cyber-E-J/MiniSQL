8142c2fb3dac2c379cebdab4343a925f4f6d7731
李世钰 <3200102893@zju.edu.cn>
2022-06-05 10:45:40 +0000

    Replace table.cpp

--- src/catalog/table.cpp
+++ src/catalog/table.cpp
@@ -1,31 +1,86 @@
-#include "catalog/table.h"
+#ifndef MINISQL_TABLE_H
+#define MINISQL_TABLE_H
 
-uint32_t TableMetadata::SerializeTo(char *buf) const {
-  return 0;
-}
+#include <memory>
 
-uint32_t TableMetadata::GetSerializedSize() const {
-  return 0;
-}
+#include "glog/logging.h"
+#include "record/schema.h"
+#include "storage/table_heap.h"
+
+class TableMetadata {
+  friend class TableInfo;
+
+public:
+  uint32_t SerializeTo(char *buf) const;
+
+  uint32_t GetSerializedSize() const;
+
+  static uint32_t DeserializeFrom(char *buf, TableMetadata *&table_meta, MemHeap *heap);
+
+  static TableMetadata *Create(table_id_t table_id, std::string table_name,
+                               page_id_t root_page_id, TableSchema *schema, MemHeap *heap);
+
+  inline table_id_t GetTableId() const { return table_id_; }
+
+  inline std::string GetTableName() const { return table_name_; }
+
+  inline uint32_t GetFirstPageId() const { return root_page_id_; }
+
+  inline Schema *GetSchema() const { return schema_; }
+
+
+private:
+  TableMetadata() = delete;
+
+  TableMetadata(table_id_t table_id, std::string table_name, page_id_t root_page_id, TableSchema *schema);
+
+private:
+  static constexpr uint32_t TABLE_METADATA_MAGIC_NUM = 344528;
+  table_id_t table_id_;
+  std::string table_name_;
+  page_id_t root_page_id_;
+  Schema *schema_;
+};
 
 /**
- * @param heap Memory heap passed by TableInfo
+ * The TableInfo class maintains metadata about a table.
  */
-uint32_t TableMetadata::DeserializeFrom(char *buf, TableMetadata *&table_meta, MemHeap *heap) {
-  return 0;
-}
+class TableInfo {
+  friend class CatalogManager;
+public:
+  static TableInfo *Create(MemHeap *heap) {
+    void *buf = heap->Allocate(sizeof(TableInfo));
+    return new(buf)TableInfo();
+  }
 
-/**
- * Only called by create table
- *
- * @param heap Memory heap passed by TableInfo
- */
-TableMetadata *TableMetadata::Create(table_id_t table_id, std::string table_name,
-                                     page_id_t root_page_id, TableSchema *schema, MemHeap *heap) {
-  // allocate space for table metadata
-  void *buf = heap->Allocate(sizeof(TableMetadata));
-  return new(buf)TableMetadata(table_id, table_name, root_page_id, schema);
-}
+  ~TableInfo() {
+    delete heap_;
+  }
 
-TableMetadata::TableMetadata(table_id_t table_id, std::string table_name, page_id_t root_page_id, TableSchema *schema)
-        : table_id_(table_id), table_name_(table_name), root_page_id_(root_page_id), schema_(schema) {}
+  void Init(TableMetadata *table_meta, TableHeap *table_heap) {
+    table_meta_ = table_meta;
+    table_heap_ = table_heap;
+  }
+
+  inline TableHeap *GetTableHeap() const { return table_heap_; }
+
+  inline MemHeap *GetMemHeap() const { return heap_; }
+
+  inline table_id_t GetTableId() const { return table_meta_->table_id_; }
+
+  inline std::string GetTableName() const { return table_meta_->table_name_; }
+
+  inline Schema *GetSchema() const { return table_meta_->schema_; }
+
+  inline page_id_t GetRootPageId() const { return table_meta_->root_page_id_; }
+
+private:
+  explicit TableInfo() : heap_(new SimpleMemHeap()) {};
+
+private:
+  TableMetadata *table_meta_;
+  TableHeap *table_heap_;
+  MemHeap *heap_; /** store all objects allocated in table_meta and table heap */
+};
+
+#endif //MINISQL_TABLE_H
